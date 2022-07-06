f365968b3fc71d75bbb56a9b5eb949519d3029b8
IzanDuesoLopez <izandueso@gmail.com>
2022-07-06 21:29:15 +0200

    EjercicioCuatroApp

---
+++ TA04 - Ejercicio 4/src/ejercicio4/EjercicioCuatroApp.java
@@ -0,0 +1,22 @@
+package ejercicio4;
+
+public class EjercicioCuatroApp {
+
+	public static void main(String[] args) {
+
+		int n = 100;
+		int resultado = 0;
+
+		System.out.println("Valor inicial de N = " + n);
+
+		resultado = n + 77;
+		System.out.println("N + 77 = " + resultado); // Mostramos por consola el resultado de N + 77
+
+		resultado = n - 3;
+		System.out.println("N - 3 = " + resultado); // Mostramos por consola el resultado de N - 3
+
+		resultado = n * 2;
+		System.out.println("N * 2 = " + resultado); // Mostramos por consola el resultado de N * 2
+	}
+
+}
